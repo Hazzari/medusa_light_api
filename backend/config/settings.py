9fc73ff08ccf75275de67db6f3a052c45af53c68
Aleksandr Petrushkin <petrushkin@plarin.net>
2022-02-26 21:15:32 +0300

    bugfix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,11 +107,11 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 if DEBUG:
     STATIC_DIR = BASE_DIR / 'static'
     STATICFILES_DIRS = [str(BASE_DIR / 'static'), ]
 else:
-    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELwD = 'django.db.models.BigAutoField'
